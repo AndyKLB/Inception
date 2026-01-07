261fbab4b5159b089d428d87dbda5046e099c479
Andy Kammerer <ankammer@f1br6s17.clusters.42paris.fr>
2026-01-07 16:59:28 +0100

    inception

---
+++ srcs/requirements/mariadb/tools/init.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+MYSQL_PASSWORD=$(cat /run/secrets/db_password.txt)
+MYSQL_ROOT_PASSWORD=$(cat /run/secrets/db_root_password.txt)
+
+mkdir -p /run/mysqld
+chown mysql:mysql /run/mysqld
+chown -R mysql:mysql /var/lib/mysql
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    mysqld --user=mysql --skip-grant-tables &
+    sleep 3
+    mysql << EOF
+FLUSH PRIVILEGES;
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE IF EXISTS test;
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
+    kill $(pgrep mysqld)
+    sleep 2
+fi
+
+exec "$@"
+
+# /run/secrets -> dossier de stockage tmp pour les secrets (defini dans docker compose)
+# chown necessaire car mysqld s execute avec l user mysql qui n est pas root et doit pouvoir ecrire dans ces fichiers
+# if -> verifie si la base de donnee existe sinon il l initialise --user -> qui execute datadir -> ou creer les fichiers
+# --skip-grant-tables -> mode special pour exec SQL temporaire sans demarrer completement (pid quelconque) -> '&' = background 
+    #commmande SQL:
+        # DELETE ... -> supprime les users anonymes (qui pouvaient se connecter sans MDP)
+        # DROP ... -> supprime "test" qui est une base accessible a tous et inutile en prod
+        # CREATE DATABASE ... -> cree base de donnee si ell n existe pas avec la var env $MYSQL...
+        # CREATE USER ... -> cree un user, '@''%' -> depuis n importe quelle ip('@''localhost' -> seulement dans le meme conteneur), identifie par nom et MDP (.env)
+        # GRANT ... -> donne tout les droits sur la base a cet user (.env)
+        # ALTER ... -> modifie un user, par defaut root n a pas de MDP on lui en attribue un
+        # FLUSH ... -> applique tout les changements de droit immediatement (GRANT)
+# kill -> kill le processus mysqld tmp (pgrep -> ps | grep)
---
+++ srcs/requirements/wordpress/tools/init.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+chown -R www-data:www-data /var/www/html
+
+cd /var/www/html
+
+if [ ! -f wp-config.php ]; then
+
+    readarray -t WP_PASS < /run/secrets/credentials.txt
+    WP_ADMIN_PASSWORD=$(WP_PASS[0])
+    WP_USER_PASSWORD=$(WP_PASS[1])
+
+    wp core download --allow-root
+
+    wp config create --allow-root \
+        --dbname=$MY_SQL_DATABASE \
+        --dbuser=$MYSQL_USER \
+        --dbpassword=$MYSQL_PASSWORD \
+        --dbhost=$MYSQL_HOST
+
+    wp core install --allow-root \
+        --url=$DOMAIN_NAME \
+        --title=$WP_TITLE \
+        --admin_user=$WP_ADMIN_USER \
+        --admin_password=$WP_ADMIN_PASSWORD \
+        --admin_email=$WP_ADMIN_EMAIL
+
+    wp user create $WP_USER $WP_USER_EMAIL \
+        --role=author \
+        --user_pass=$WP_USER_PASSWORD \
+        --allow-root
+
+    chown -R www-data:www-data /var/www/html
+
+fi
+
+exec "$@"
+
+# pas set -e pour avoir des logs de wp wt pouvoir debugger
+# readarray -> lit ligne par ligne et stocke dasn un array -t -> suppr '\n' de fin
+# /run/secrets -> dossier de stockage tmp pour les secrets (defini dans docker compose)
+# www-data -> user et group par defaut
+# wp core download -> installe WP
+# wp config create -> creer wp-config.php
+# wp core install -> creer database par defaut (12 tables)
+# wp user create -> creer 2e user comme requis par le sujet
